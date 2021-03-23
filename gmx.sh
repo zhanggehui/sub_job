ba5939f2e0f72bdd829e6ea266098805becf8ab0
zhanggehui <395510548@qq.com>
2021-03-23 23:22:26 +0800

    update

--- gmx.sh
+++ gmx.sh
@@ -4,14 +4,14 @@
 Usempirun=1   # mdrun_mpi or mdrun
 # export I_MPI_DEBUG=20
 if [ $SLURM_JOB_PARTITION == 'cn_nl' ]; then
-    echo 'Choose gromacs2019.2_intelmkl2019u4 ( for cn_nl ) !' 
+    echo 'Choose gromacs2019.2_intelmkl2019u4 (for cn_nl)!' 
     source /appsnew/mdapps/gromacs2019.2_intelmkl2019u4/bin/GMXRC2.bash
 else
     # 2020的环境变量，但是有没装mdrun_mpi
     # export LD_LIBRARY_PATH=/appsnew/usr/gcc/gcc-7.4.0/lib64:$LD_LIBRARY_PATH
     # source /appsnew/source/intel2019.sh
     # source /appsnew/mdapps/gromacs2020_cpu_intelmkl2019_cnscompat/bin/GMXRC.bash
-    echo 'Choose gromacs2019.3_cpu_intelmkl2019_cnscompat ( for cn-short ) !' 
+    echo 'Choose gromacs2019.3_cpu_intelmkl2019_cnscompat(for cn-short)!' 
     source /appsnew/mdapps/gromacs2019.3_cpu_intelmkl2019_cnscompat/bin/GMXRC2.bash
 fi
 
